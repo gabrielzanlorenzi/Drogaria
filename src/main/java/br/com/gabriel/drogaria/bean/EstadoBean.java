9abf62989b15cecc86f656b56364e9ca1c476f0a
Gabriel Zanlorenzi <gabriel.zanlorenzi1@gmail.com>
2016-03-06 01:15:19 -0300

    Litando, salvando, setando novo estado

--- src/main/java/br/com/gabriel/drogaria/bean/EstadoBean.java
+++ src/main/java/br/com/gabriel/drogaria/bean/EstadoBean.java
@@ -1,8 +1,16 @@
 package br.com.gabriel.drogaria.bean;
 
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import org.omnifaces.util.Messages;
+
+import br.com.gabriel.drogaria.dao.EstadoDAO;
+import br.com.gabriel.drogaria.domain.Estado;
 
 /*
  * Classe EstadoBean para se comunicar com a parte visual
@@ -11,17 +19,60 @@ import org.omnifaces.util.Messages;
  */
 
 @ManagedBean
-public class EstadoBean {
+@ViewScoped
+public class EstadoBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Estado estado;
+	private List<Estado> estados;
+
+	public void novo() {
+		estado = new Estado();
+	}
 
 	public void salvar() {
-//		System.out.println("Salvo!");
-//		String texto = "Salvo!";
-//		FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_INFO, texto, texto);
-//
-//		FacesContext contexto = FacesContext.getCurrentInstance();
-//		contexto.addMessage(null, mensagem);
-		
-		Messages.addGlobalInfo("Estado Salvo!");
+
+		try {
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estadoDAO.salvar(estado);
+
+			novo();
+
+			Messages.addGlobalInfo("Estado foi salvo!");
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar salvar o estado");
+			erro.printStackTrace();
+		}
+	}
+
+	@PostConstruct // Como se fosse um construtor (Chama automaticamente o método listar)
+	public void listar() {
+		try {
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estados = estadoDAO.listar();
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar os estados");
+			erro.printStackTrace();
+		}
+	}
+
+	// Sessão de get / set
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+	public List<Estado> getEstados() {
+		return estados;
+	}
+
+	public void setEstados(List<Estado> estados) {
+		this.estados = estados;
 	}
 
 }

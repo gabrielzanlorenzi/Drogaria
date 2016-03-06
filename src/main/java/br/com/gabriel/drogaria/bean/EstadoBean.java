a21f4218dd9a7aa19d77406a296615703b4cd9dd
Gabriel Zanlorenzi <gabriel.zanlorenzi1@gmail.com>
2016-03-06 11:01:39 -0300

    Excluindo o estado

--- src/main/java/br/com/gabriel/drogaria/bean/EstadoBean.java
+++ src/main/java/br/com/gabriel/drogaria/bean/EstadoBean.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Messages;
 
@@ -38,6 +39,7 @@ public class EstadoBean implements Serializable {
 			estadoDAO.salvar(estado);
 
 			novo();
+			listar();
 
 			Messages.addGlobalInfo("Estado foi salvo!");
 		} catch (RuntimeException erro) {
@@ -46,13 +48,30 @@ public class EstadoBean implements Serializable {
 		}
 	}
 
-	@PostConstruct // Como se fosse um construtor (Chama automaticamente o método listar)
+	@PostConstruct // Como se fosse um construtor (Chama automaticamente o
+					// método listar)
 	public void listar() {
 		try {
 			EstadoDAO estadoDAO = new EstadoDAO();
 			estados = estadoDAO.listar();
 		} catch (RuntimeException erro) {
-			Messages.addGlobalError("Ocorreu um erro ao tentar listar os estados");
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar os estados!");
+			erro.printStackTrace();
+		}
+	}
+
+	public void excluir(ActionEvent evento) {
+		try {
+			estado = (Estado) evento.getComponent().getAttributes().get("estadoSelecionado");
+
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estadoDAO.excluir(estado);
+			
+			listar();
+
+			Messages.addGlobalInfo("Estado removido com sucesso!");
+		} catch (RuntimeException erro) {
+			Messages.addFlashGlobalError("Ocorreu um erro ao tentar remover o estado!");
 			erro.printStackTrace();
 		}
 	}
